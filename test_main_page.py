07b315846a1c6f0c13dd34c0d43212100218f751
Daniil <kit-91-daniil@mail.ru>
2021-11-18 20:36:40 +0300

    add moving to login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.locators import MainPageLocators
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,7 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     main_page = MainPage(browser, main_page_url)
     main_page.open()
     main_page.go_to_login_page()
-    assert True
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
